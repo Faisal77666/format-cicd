17332be60b33c63e1faba2dd473b4217df569a2b
Faisal Mehmood <sfm7776@gmail.com>
2026-06-13 15:47:56 +0500

    update devops.py

--- devops.py
+++ devops.py
@@ -1,14 +1,7 @@
-def calculate_sum(a,b):
-    result=a+b
-    unused_variable = 10
+def calculate_sum(a, b):
+    result = a + b
     return result
   def greet(name):
     print("Hello " + name)
-    return None
-    def check_even(number):
-    if number % 2 == 0:
-        return True
-    else:
-        return False
-      def check_even(number):
+    ddef check_even(number):
     return number % 2 == 0
